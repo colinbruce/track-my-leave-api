c9da00807689789c239866bff5574b06ae075f19
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-03-02 11:45:39 +0000

    Add authentication method to application controller
    We can now add the `authenticate_request` method as a
    `before_filter` to any controller action that we want
    to be authenticated for the user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::API
+
+  protected
+
+  def authenticate_request!
+    if !payload || !JsonWebToken.valid_payload(payload)
+      return invalid_authentication
+    end
+
+    load_current_user!
+    invalid_authentication unless @current_user
+  end
+
+  def invalid_authentication
+    render json: { error: 'Invalid request', status: :unauthorized }
+  end
+
+  private
+
+  def payload
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last
+    JsonWebToken.decode(token).first
+  end
+
+  def load_current_user!
+    @current_user = User.find(payload['user_id'])
+  end
 end
